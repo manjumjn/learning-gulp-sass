fd02def2197a38e6f93d723a92ea07d8b13addc8
Raunak Hajela <raunakhajela@gmail.com>
2018-09-25 14:34:32 +0530

    Install and Greetings

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+
+//'default' is a task name
+gulp.task('default', function(){
+    console.log('Hello Gulp');
+});
+
+//to run the gulpfile simple type 'gulp' in console, it will automatically look for gulp file and run a task
